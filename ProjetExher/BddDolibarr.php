9c42131c583740e78cd629d14deee2f2c4c4a92e
Blaccreed <62603834+Blaccreed@users.noreply.github.com>
2021-01-21 16:25:59 +0100

    Correction

--- ProjetExher/BddDolibarr.php
+++ ProjetExher/BddDolibarr.php
@@ -1,12 +1,14 @@
 <?php
+include("fonctions.php");
 $con = BddConnect();
-$requete = "SELECT * FROM llx_adherent";
+$requete = "SELECT lastname, firstname, address, zip, town, email,phonemobile FROM llx_adherent";
 $resultat = mysqli_query($requete,$con);
-$file = fopen("fichier.txt","w");
+$file = fopen("f.txt","w");
 while($tableau = mysqli_fetch_array($resultat))
 {
-  //aucune récuépration
+  $adher =$ligne['lastname'].",".$tableau['firstname'].",".$tableau['address'].",".$tableau['zip'].",".$tableau['town'].",".$tableau['email'].",".$tableau["phonemobile"]."\n";
+  fwrite($file,$adher);
 }
-
+fclose($file);
 
  ?>
